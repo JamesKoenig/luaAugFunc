dd4062907581e32e47086f1c972fcac29f29fc0b
James Koenig <Koenig.James@live.com>
2024-04-27 12:57:32 -0700

    1st-step refactor for better utility in 5.1/etc
    starting to move functions so that they can be called with existing
    object:method OOP syntax, which will allow people to either use the
    function augmentations when certain metamethods aren't supported by the
    interpreter, or when the oop syntax would simply suit the code better

--- augFunc.lua
+++ augFunc.lua
@@ -31,90 +31,111 @@
 --  wraps its own constructor
 --]]
 
-local mt = {
-  --af(...) -- calls f(...)
-  __call = function(aft,...)
-    return aft.f(...)
-  end,
+-- generate a table of util methods for the augmented function
+--  note that `aft` is used here to refer to the table itself
+--   itself standing for 'augmented function table'
+local function genUtilsTable(func)
+  return {
+    f = func,
 
-  --f .. g -- composition: ((f..g)(x) is f'(g(x))), returns an augmented fn
-  __concat = function(aft,fn)
-    return augFunction(function(...)
-      return aft.f(fn(...))
-    end)
-  end,
+    -- aft:call(...)
+    call = function(aft,...)
+      return aft.f(...)
+    end,
 
+    -- aft:compose(fn)
+    compose = function(aft,fn)
+      return augFunction(function(...)
+        return aft.f(fn(...))
+      end)
+    end,
 
-  --f * foo -- map f' over foo, returns the result as a table
-  __mul = function(aft,nextable)
-    res = {}
-    for key,value in pairs(nextable) do
-      res[key] = aft.f(value)
-    end
-    return res
-  end,
-
-  --f << foo -- return first element of foo for which f' does not return nil
-  __shl = function(aft, nextable)
-    for _,value in pairs(nextable) do
-      local testVal = aft.f(value)
-      if testVal then
-        return testVal
-      end
-    end
-    return nil
-  end,
-
-  --f ^ g -- return an augmented functiont hat maps f' over the results of g
-  __pow = function(aft, fn)
-    return augFunction(function(...)
+    -- aft:map(nextable)
+    map = function(aft,tableLike)
       res = {}
-      for key,value in pairs(fn(...)) do
+      for key, value in pairs(tableLike) do
         res[key] = aft.f(value)
       end
       return res
-    end)
-  end,
-
-  -- f ~ foo -- filter elements of foo where f'(ele) ~= nil
-  __bnot = function(aft, nextable)
-    res = {}
-    for key,value in pairs(nextable) do
-      if aft.f(value) ~= nil then
-        res[key] = value
-      end
     end
-    return res
-  end,
+  }
+end
 
-  -- f & g -- returns an augmented fn that executes f, & if f doesn't return nil
-  --       --  it returns the result of g on the same arguments
-  __band = function(aft, fn)
-    return augFunction(function(...)
-      if(aft.f(...) ~= nil) then
-        return fn(...)
-      else
-        return nil -- this was implied but I felt better adding it explicitly
-      end
-    end)
-  end,
+local function genMetaTable(aft)
+  return {
+    --af(...) -- calls f(...)
+    __call = aft.call,
 
-  -- f | g -- returns an augmented fn that that executes f, & if f returns nil
-  --       --  it returns the result of g on the same arguments
-  __bor = function(aft, fn)
-    return augFunction(function(...)
-      local maybe = aft.f(...)
-      if maybe == nil then
-        return fn(...)
-      else
-        return maybe
+    --f .. g -- composition: ((f..g)(x) is f'(g(x))), returns an augmented fn
+    __concat = aft.compose,
+
+    --f * foo -- map f' over foo, returns the result as a table
+    __mul = aft.map,
+
+    --f << foo -- return first element of foo for which f' does not return nil
+    __shl = function(aft, nextable)
+      for _,value in pairs(nextable) do
+        local testVal = aft.f(value)
+        if testVal then
+          return testVal
+        end
       end
-    end)
-  end,
-}
+      return nil
+    end,
+
+    --f ^ g -- return an augmented function that maps f' over the results of g
+    __pow = function(aft, fn)
+      return augFunction(function(...)
+        res = {}
+        for key,value in pairs(fn(...)) do
+          res[key] = aft.f(value)
+        end
+        return res
+      end)
+    end,
+
+    -- f ~ foo -- filter elements of foo where f'(ele) ~= nil
+    __bnot = function(aft, nextable)
+      res = {}
+      for key,value in pairs(nextable) do
+        if aft.f(value) ~= nil then
+          res[key] = value
+        end
+      end
+      return res
+    end,
+
+    -- f & g -- returns an augmented fn that calls f, & if f doesn't return nil
+    --       --  it returns the result of g on the same arguments
+    __band = function(aft, fn)
+      return augFunction(function(...)
+        if(aft.f(...) ~= nil) then
+          return fn(...)
+        else
+          return nil -- this was implied but I felt better adding it explicitly
+        end
+      end)
+    end,
+
+    -- f | g -- returns an augmented fn that that executes f, & if f returns nil
+    --       --  it returns the result of g on the same arguments
+    __bor = function(aft, fn)
+      return augFunction(function(...)
+        local maybe = aft.f(...)
+        if maybe == nil then
+          return fn(...)
+        else
+          return maybe
+        end
+      end)
+    end,
+  }
+end
 
 function augFunction(fn)
-  return setmetatable({f = fn}, mt)
+  local utilTable = genUtilsTable(fn)
+  local metaTable = genMetaTable(utilTable)
+  return setmetatable(utilTable, metaTable)
 end
 
 return augFunction(augFunction)
