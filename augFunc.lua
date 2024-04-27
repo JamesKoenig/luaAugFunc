967461f368fb5a28f89ab4fc4f7b5c66116860c9
James Koenig <Koenig.James@live.com>
2024-04-27 11:16:08 -0700

    added documentation, added band/bor, removed >>
    f >> g and f & g ended up being synonymous after some code drafting.
    
    I'm still not sure about the idea of the | and & operating on the same
    arguments, but it's still useful as is.

--- augFunc.lua
+++ augFunc.lua
@@ -1,8 +1,35 @@
--- TODO the following:
---f | g    -- returns a metafunction which runs f'(...), returns its result on success, but returns the result of g(x) on failure
---f & g    -- returns a metafunction which returns nil if either f(...) or g(...) return nil, running f first
-
-
+--[[
+-- defines a metatable that grants some shorthand rules of composition and
+--   utility for a given function.
+--
+-- the wrapped function **must** return nil on failure, and any value on success
+--
+-- the result function-wrapping metatable is called an augmented function 
+--   or augFn in this documentation
+--
+-- further documentation is available in the readme and at the end of the file.
+--
+-- the augmented function metatable defines the operations:
+--  calling:
+--    f()
+--  composition:
+--    f .. g
+--  mapping:
+--    f * tableLike
+--  selection:
+--    f << tableLike
+--  binding of a an augmented function to a mappable result of another:
+--    f ^ g
+--  filtering:
+--    f ~ tableLike
+--  and-conditional-sequencing (f then g on success of f):
+--    f & g
+--  or-conditional-sequencing (f then g on failure of f):
+--    f | g
+--
+-- the result of `require`-ing this file is itself an augmented function which
+--  wraps its own constructor
+--]]
 
 local mt = {
   --af(...) -- calls f(...)
@@ -35,6 +62,7 @@ local mt = {
         return testVal
       end
     end
+    return nil
   end,
 
   --f ^ g -- return an augmented functiont hat maps f' over the results of g
@@ -59,12 +87,27 @@ local mt = {
     return res
   end,
 
-  --f >> g -- f'(...) then g(...) on the same args if f is not nil, 
-  --returning the results of g's evaluation
-  __shr = function(aft, fn)
+  -- f & g -- returns an augmented fn that executes f, & if f doesn't return nil
+  --       --  it returns the result of g on the same arguments
+  __band = function(aft, fn)
     return augFunction(function(...)
       if(aft.f(...) ~= nil) then
         return fn(...)
+      else
+        return nil -- this was implied but I felt better adding it explicitly
+      end
+    end)
+  end,
+
+  -- f | g -- returns an augmented fn that that executes f, & if f returns nil
+  --       --  it returns the result of g on the same arguments
+  __bor = function(aft, fn)
+    return augFunction(function(...)
+      local maybe = aft.f(...)
+      if maybe == nil then
+        return fn(...)
+      else
+        return maybe
       end
     end)
   end,
